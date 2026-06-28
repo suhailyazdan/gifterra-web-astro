8fbccf3cefdb19a948805e726b5d6199d8ea48fb
suhailyazdan <yazdan.suhail@gmail.com>
2026-06-28 23:35:30 +0530

    wranger is removed

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,8 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
-import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
-  adapter: cloudflare(),
-
   vite: {
     plugins: [tailwindcss()],
   },
